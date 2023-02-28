0a7f629f408dffee9fce2d2ebe4b14da7c84cf0d
ferbogda <126582666+ferbogda@users.noreply.github.com>
2023-02-28 10:50:36 -0300

    Add files via upload

---
+++ proyecto_final_CV.js
@@ -0,0 +1,40 @@
+
+function mouseOver1() {
+    document.getElementById("informacion_personal").style.color = "blue";
+  }
+  
+  function mouseOut1() {
+    document.getElementById("informacion_personal").style.color = "black";
+  }
+
+  function mouseOver2() {
+    document.getElementById("estudios").style.color = "blue";
+  }
+  
+  function mouseOut2() {
+    document.getElementById("estudios").style.color = "black";
+  }
+
+  function mouseOver3() {
+    document.getElementById("experiencia_laboral").style.color = "blue";
+  }
+  
+  function mouseOut3() {
+    document.getElementById("experiencia_laboral").style.color = "black";
+  }
+
+  function mouseOver4() {
+    document.getElementById("habilidades").style.color = "blue";
+  }
+  
+  function mouseOut4() {
+    document.getElementById("habilidades").style.color = "black";
+  }
+
+  function mouseOver5() {
+    document.getElementById("espectativas").style.color = "blue";
+  }
+  
+  function mouseOut5() {
+    document.getElementById("espectativas").style.color = "black";
+  }
